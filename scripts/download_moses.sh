9f729bba73763c0141590731796bd3c31f461a4d
lucasseiler <45484429+lucasseiler@users.noreply.github.com>
2021-05-04 15:46:22 +0200

    Update download_moses.sh

--- scripts/download_moses.sh
+++ scripts/download_moses.sh
@@ -6,6 +6,6 @@ base=$scripts/..
 tools=$base/tools
 mkdir -p $tools
 
-# install Moses scripts for preprocessing
+# install Moses scripts for postprocessing
 
 git clone https://github.com/bricksdont/moses-scripts $tools/moses-scripts
